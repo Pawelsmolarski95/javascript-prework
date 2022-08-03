a37fbd8538b3a6752edfb7389e6c8aeebeea0333
Pawel Smolarski <smolenc1@gmail.com>
2022-08-03 09:11:36 +0200

    add-variables-javascript-prework

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+
+// function
+
+function printMessage(msg) {
+    let giv = document.createElement('div');
+    document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages() {
+    document.getElementById('messages').innerHTML = '';
+}
+
+printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+
+
+// variables
+
+let computerMove = 'kamień';
+let playerMove = 'papier '
