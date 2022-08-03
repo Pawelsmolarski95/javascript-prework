80f36d130b42ee13b1abd2989c38bec215f68a59
Pawel Smolarski <smolenc1@gmail.com>
2022-08-03 11:42:24 +0200

    javascript-prework-add-function

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,2 @@
 
-// function
 
-function printMessage(msg) {
-    let giv = document.createElement('div');
-    document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages() {
-    document.getElementById('messages').innerHTML = '';
-}
-
-printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
-
-
-// variables
-
-let computerMove = 'kamień';
-let playerMove = 'papier ';
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
-
-
-
-// losowanie liczby 
